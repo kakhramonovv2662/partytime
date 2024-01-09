9763c8e45e678746a3d27d4f8df23d790be3b883
Kakhramonovv Ibrohim <94061638+kakhramonovv2662@users.noreply.github.com>
2024-01-09 11:41:20 +0500

    fixed submit button

--- src/components/layouts/button/button.jsx
+++ src/components/layouts/button/button.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom';
 
-export const Button = ({ text, route = '/', className }) => {
+export const Button = ({ text, route = '/', className, type = 'button' }) => {
   return (
     <button
+      type={type}
       className={`text-xs font-semibold leading-3 tracking-[2px] uppercase whitespace-nowrap justify-center items-center border bg-gray-200 px-7 py-3.5 rounded-lg transition-all border-solid border-gray-200 hover:text-white ${className}`}
     >
       <Link href={route}>{text}</Link>
